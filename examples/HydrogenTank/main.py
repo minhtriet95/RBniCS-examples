90e6e1420f294e2eb0863dd07e7a17fa1fee0e76
minhtriet95 <minhtriet240@gmail.com>
2021-02-18 17:45:32 +0900

    Update main.py

--- examples/HydrogenTank/main.py
+++ examples/HydrogenTank/main.py
@@ -1,7 +1,13 @@
 from dolfin import *
 from rbnics import *
+import timeit
 from subfiles.clear_offline_data import Cleaner
 
+start_time = timeit.default_timer()
+
+# Units
+GPa = 10**9
+MPa = 10**6
 
 # Define parameters for mapping functions
 offset = "1.5131"
@@ -14,27 +20,26 @@ mapping_y_sub1 = f"x[1] + (0.05-mu[0])*x[1]/0.2294*(-20*{x_norm_sub1}+1397./250)
 mapping_z_sub1 = "x[2]"
 
 # For left spherical head (subdomain 2)
-z_sub2 = f"x[2] + {offset}"
-x_norm_sub2 = f"sqrt(x[0]**2 + x[1]**2 + {z_sub2}**2)"
+z_sub2 = "(x[2] + 1.5131)"
+x_norm_sub2 = "sqrt(x[0]**2 + x[1]**2 + (x[2] + 1.5131)**2)"
+# x_norm_sub2 = "sqrt(x[0]**2 + x[1]**2 + x[2]**2)"
 mapping_x_sub2 = f"x[0] + (0.05-mu[0])*x[0]/0.2294*(-20*{x_norm_sub2}+1397./250) + (mu[1]-0.2794)*x[0]/{x_norm_sub2}"
 mapping_y_sub2 = f"x[1] + (0.05-mu[0])*x[1]/0.2294*(-20*{x_norm_sub2}+1397./250) + (mu[1]-0.2794)*x[1]/{x_norm_sub2}"
-mapping_z_sub2 = f"{z_sub2} + (0.05-mu[0])*{z_sub2}/0.2294*(-20*{x_norm_sub2}+1397./250) + (mu[1]-0.2794)*{z_sub2}/{x_norm_sub2} - {offset}"
+mapping_z_sub2 = f"{z_sub2} + (0.05-mu[0])*{z_sub2}/0.2294*(-20*{x_norm_sub2}+1397./250) + (mu[1]-0.2794)*{z_sub2}/{x_norm_sub2} - 1.5131"
 
 # For right spherical head (subdomain 3)
-z_sub3 = f"x[2] - {offset}"
-x_norm_sub3 = f"sqrt(x[0]**2 + x[1]**2 + {z_sub3}**2)"
+z_sub3 = "(x[2] - 1.5131)"
+x_norm_sub3 = "sqrt(x[0]**2 + x[1]**2 + (x[2] - 1.5131)**2)"
 mapping_x_sub3 = f"x[0] + (0.05-mu[0])*x[0]/0.2294*(-20*{x_norm_sub3}+1397./250) + (mu[1]-0.2794)*x[0]/{x_norm_sub3}"
 mapping_y_sub3 = f"x[1] + (0.05-mu[0])*x[1]/0.2294*(-20*{x_norm_sub3}+1397./250) + (mu[1]-0.2794)*x[1]/{x_norm_sub3}"
-mapping_z_sub3 = f"{z_sub3} + (0.05-mu[0])*{z_sub3}/0.2294*(-20*{x_norm_sub3}+1397./250) + (mu[1]-0.2794)*{z_sub3}/{x_norm_sub3} + {offset}"
+mapping_z_sub3 = f"{z_sub3} + (0.05-mu[0])*{z_sub3}/0.2294*(-20*{x_norm_sub3}+1397./250) + (mu[1]-0.2794)*{z_sub3}/{x_norm_sub3} + 1.5131"
 
 # For nozzle (subdomain 4)
-z_sub4 = f"x[2] - {offset}"
-x_norm_sub4 = f"sqrt(x[0]**2 + x[1]**2 + {z_sub4}**2)"
+z_sub4 = "(x[2] - 1.5131)"
+x_norm_sub4 = "sqrt(x[0]**2 + x[1]**2 + (x[2] - 1.5131)**2)"
 mapping_x_sub4 = f"x[0] + (mu[1] - 0.2794)*x[0]/{x_norm_sub4}"
 mapping_y_sub4 = f"x[1] + (mu[1] - 0.2794)*x[1]/{x_norm_sub4}"
-mapping_z_sub4 = f"{z_sub4} + (mu[1] - 0.2794)*{z_sub4}/{x_norm_sub4} + {offset}"
-
-print("Mapping functions: done")
+mapping_z_sub4 = f"{z_sub4} + (mu[1] - 0.2794)*{z_sub4}/{x_norm_sub4} + 1.5131"
 
 @DEIM()
 @PullBackFormsToReferenceDomain()
@@ -56,12 +61,12 @@ class ElasticBlock(EllipticCoerciveProblem):
         self.subdomains, self.boundaries = kwargs["subdomains"], kwargs["boundaries"]
         self.u = TrialFunction(V)
         self.v = TestFunction(V)
+        self.n = FacetNormal(mesh)
         self.dx = Measure("dx")(subdomain_data=self.subdomains)
         self.ds = Measure("ds")(subdomain_data=self.boundaries)
         # ...
-        self.f = Constant((0.0, 1.0, 0.0))
-        self.E = 1.0
-        self.nu = 0.3
+        self.E = 201.0*GPa
+        self.nu = 0.306
         self.lambda_1 = self.E * self.nu / ((1.0 + self.nu) * (1.0 - 2.0 * self.nu))
         self.lambda_2 = self.E / (2.0 * (1.0 + self.nu))
 
@@ -83,7 +88,7 @@ class ElasticBlock(EllipticCoerciveProblem):
             theta_a3 = 1.0
             return (theta_a0, theta_a1, theta_a2, theta_a3)
         elif term == "f":
-            theta_f0 = 10.0
+            theta_f0 = -40.0*MPa
             return (theta_f0, )
         else:
             raise ValueError("Invalid term for compute_theta().")
@@ -101,11 +106,13 @@ class ElasticBlock(EllipticCoerciveProblem):
             return (a0, a1, a2, a3)
         elif term == "f":
             ds = self.ds
-            f = self.f
-            f0 = inner(f, v) * ds(2)
+            n = self.n
+            f0 = dot(v, n) * ds(4)
             return (f0, )
         elif term == "dirichlet_bc":
-            bc0 = [DirichletBC(self.V, Constant((0.0, 0.0, 0.0)), self.boundaries, 3)]
+            bc0 = [DirichletBC(self.V, Constant((0.0, 0.0, 0.0)), self.boundaries, 1),  # fully fixed (u, v, w = 0)
+                   DirichletBC(self.V.sub(0), Constant(0.0), self.boundaries, 2),       # displacement u = 0
+                   DirichletBC(self.V.sub(1), Constant(0.0), self.boundaries, 3)]       # displacement v = 0
             return (bc0,)
         elif term == "inner_product":
             u = self.u
@@ -127,40 +134,30 @@ boundaries = MeshFunction("size_t", mesh, "data/vessel_facet_region.xml")
 V = VectorFunctionSpace(mesh, "Lagrange", 1)
 
 problem = ElasticBlock(V, subdomains=subdomains, boundaries=boundaries)
-
-print("Create instance: done")
-
-mu_range = [(0.04, 0.06), (0.27, 0.28)]
+mu_range = [(0.0267, 0.0802), (0.1362, 0.4087)]
 problem.set_mu_range(mu_range)
 
 reduction_method = ReducedBasis(problem)
+reduction_method.set_Nmax(30)
+reduction_method.set_tolerance(1e-5)
 
-print("Set training parameters")
-
-reduction_method.set_Nmax(30, DEIM=20)
-reduction_method.set_tolerance(1e-5, DEIM=1e-3)
-
-print("Create training set")
 solve_stage = "offline"
-reduction_method.initialize_training_set(100, DEIM=25)
-# Delete old offline data
-if solve_stage == "offline":
-    clean = Cleaner(problem.name())
-    names = clean.list_names(clean.path)
-    if problem.name() in names:
-        clean.delete_offline_folder()
-elif solve_stage == "online":
-    pass
-else:
-    print("Invalid name for solving stage!")
+reduction_method.initialize_training_set(100)
 
-print("Begin offline training")
+# Delete old offline data
+clean = Cleaner(problem.name())
+names = clean.list_names(clean.path)
+clean.delete_offline_folder() if solve_stage == "offline" and problem.name() in names else None
+
 reduced_problem = reduction_method.offline()
 
-print("Offline training: done")
-
-online_mu = (0.04, 0.2794)
+online_mu = (0.04, 0.27)
 reduced_problem.set_mu(online_mu)
 reduced_solution = reduced_problem.solve()  # Obtain coefficient "weights"
 reduced_problem.export_solution(filename="online_solution")
 # P = plot(reduced_solution, reduced_problem=reduced_problem)
+
+stop_time = timeit.default_timer()
+run_time = round(stop_time - start_time, 2)
+
+print(f'FINISH SOLVING IN {run_time}s')
